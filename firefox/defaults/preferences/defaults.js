cc01d02cfa9d69c63a5b8d0b5ec2af9e7dd37a42
webgapps.org <labs@webgapps.org>
2011-03-08 19:08:02 -0300

    added security warning

--- firefox/defaults/preferences/defaults.js
+++ firefox/defaults/preferences/defaults.js
@@ -1,8 +1,8 @@
 pref("extensions.flashaid.terminal", "/usr/bin/x-terminal-emulator");
 pref("extensions.flashaid.needrestart", false);
 pref("extensions.flashaid.updatealert", true);
-pref("extensions.flashaid.flashbetaupdate", "{ \"flashbeta32\" : \"20101228\", \"flashbeta64\" : \"20101228\"}");
-pref("extensions.flashaid.lastflashupdate", 20101228);
+pref("extensions.flashaid.flashbetaupdate", "{ \"flashbeta32\" : \"20110308\", \"flashbeta64\" : \"20101230\"}");
+pref("extensions.flashaid.lastflashupdate", 20110308);
 pref("extensions.flashaid.hidetips", false);
 pref("extensions.flashaid.osversion", "");
 pref("extensions.flashaid.oscodename", "");
